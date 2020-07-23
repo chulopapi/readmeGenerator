3744a5251b309b6e07fb8088c0c3510c829d12a2
chulopapi <marlon.guandique@gmail.com>
2020-07-22 22:46:12 -0700

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,10 +2,16 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require("./Develop/utils/generateMarkdown");
+
 
 // array of questions for user
 const questions = [
-
+    {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+    },
 ];
 
 // function to write README file
