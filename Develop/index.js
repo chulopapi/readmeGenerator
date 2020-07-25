322d5b7764652de86a233bfa47f5cea257d55f50
chulopapi <marlon.guandique@gmail.com>
2020-07-25 16:53:37 -0700

    added function GenerateMarkDown

--- Develop/index.js
+++ Develop/index.js
@@ -3,105 +3,115 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown");
-
+console.log('THIS IS BETA')
 // Prompt the user for questions
-inquirer
-  .prompt([
+const questions = [
     {
         type: "input",
         message: "What is your project title?",
         name: "title",
     },
-    {
-        type: "input",
-        message: "Enter Description of your project: ",
-        name: "description",
-    },
-    {
-        type: "input",
-        message: "What are the installation instructions for your project?",
-        name: "installation",
-        default: "There are no specific intallation for this project”"
-    },
-    {
-        type: "input",
-        message: "Describe the use of this project?",
-        name: "title",
-    },
-    {
-      type: 'list',
-      message: 'Please choose a license you used for this project.',
-      name: 'lic',
-      choices: ['GNU GPLv3', 'GNU LGPLv3', 'Mozilla', 'MIT','Apache','Boost']
-    },
-    {
-        type: "input",
-        message: "Describe how to contribute to this project?",
-        name: "contribution",
-    },
-    {
-        type: "input",
-        message: "Please enter if any testing protocols were used for your project?",
-        name: "test",
-        default: "No test provided"
-    },
-    {
-        type: "input",
-        message: "Please enter your gitHub Username:",
-        name: "userName",
-    },
-    {
-        type: "input",
-        message: "Please enter your email address:",
-        name: "userEmail",
-    },
+    // {
+    //     type: "input",
+    //     message: "Enter Description of your project: ",
+    //     name: "description",
+    // },
+    // {
+    //     type: "input",
+    //     message: "What are the installation instructions for your project?",
+    //     name: "installation",
+    //     default: "There are no specific intallation for this project”"
+    // },
+    // {
+    //     type: "input",
+    //     message: "Describe the use of this project?",
+    //     name: "title",
+    // },
+    // {
+    //     type: 'list',
+    //     message: 'Please choose a license you used for this project.',
+    //     name: 'lic',
+    //     choices: ['GNU GPLv3', 'GNU LGPLv3', 'Mozilla', 'MIT', 'Apache', 'Boost']
+    // },
+    // {
+    //     type: "input",
+    //     message: "Describe how to contribute to this project?",
+    //     name: "contribution",
+    // },
+    // {
+    //     type: "input",
+    //     message: "Please enter if any testing protocols were used for your project?",
+    //     name: "test",
+    //     default: "No test provided"
+    // },
+    // {
+    //     type: "input",
+    //     message: "Please enter your gitHub Username:",
+    //     name: "userName",
+    // },
+    // {
+    //     type: "input",
+    //     message: "Please enter your email address:",
+    //     name: "userEmail",
+    // },
+];
 
-  ]) 
-  
-  // Writing the user response to a file by chaining the below callback method to the prompt above.
-  //.then(function(data) {
-    // Generate the name of your user file from their input
-    //const filename =
-   //   data.name
-  //      .toLowerCase()
-  //      .split(' ')
-  //      .join('') + '.json';
 
-  ////  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-  //    if (err) {
-   //     return console.log(err);
-   //   }
 
-    //  console.log('Success!');
-   // });
- // });
+// Writing the user response to a file by chaining the below callback method to the prompt above.
+//.then(function(data) {
+// Generate the name of your user file from their input
+//const filename =
+//   data.name
+//      .toLowerCase()
+//      .split(' ')
+//      .join('') + '.json';
+
+////  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+//    if (err) {
+//     return console.log(err);
+//   }
+
+//  console.log('Success!');
+// });
+// });
 
 
 // array of questions for user
 //const questions = [
-  //  {
-  //  type: "input",
-  //  message: "What is your project title?",
-  //  name: "title",
-  ////  },
+//  {
+//  type: "input",
+//  message: "What is your project title?",
+//  name: "title",
+////  },
 //];
 
 // function to write README file
 function writeToFile(fileName, data) {
     let inputData = generateMarkdown(data);
-    fs.writeFileSync(fileName,inputData,function(err){
-        if(err) {
+    fs
+        .writeFileSync(fileName, inputData, err => {
+        if (err) {
             return console(err);
         }
-    });    
+    });
 }
 
 // function to initialize program
-function init() {
-    inquirer.prompt().then((data) => {
-        console.log(JSON.stringify(data,null,2));
-        writeToFile("README.md", data);
-    });
+async function init() {
+    inquirer
+        .prompt(questions)
+        .then(data => {
+            console.log(JSON.stringify(data, null, 2));
+            writeToFile("README.md", data);
+        })
+        .catch(error => {
+            if(error.isTtyError) {
+                console.log("Prompt couldn't be rendered in the current environment")
+              // Prompt couldn't be rendered in the current environment
+            }
+            console.error('ERROR=>',error)
+        });
 }
 // function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,24 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const mapOfKeyToMarkDown = new Map([
+  ['title','#'],
+]);
 
-`;
+function generateMarkdown(data) {
+  // create a new string to return;
+  let results = "";
+  // get the keys from the data
+  const keys = Object.keys(data);
+  console.log("KEYS =>", keys);
+  // translate the keys into markdown
+  keys.forEach(key => {
+    // for each key we need the mark down and the value
+    results += `${mapOfKeyToMarkDown.get(key)} ${data[key]}\n`;
+  
+  });
+  // console.log the results
+  console.log("RESULTS =>", results);
+  // return the results
+  return results;
 }
 
 module.exports = generateMarkdown;
